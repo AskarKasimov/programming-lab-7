b38b426e2aa03a1811e73641267b3effb95eb70b
AskarKasimov <kasimovaskar123@gmail.com>
2025-04-16 19:54:13 +0300

    database

---
+++ db_init_scripts/01_init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    password_hash CHAR(96) NOT NULL
+);
+
+CREATE TYPE EventType AS ENUM ('E_SPORTS', 'FOOTBALL', 'BASKETBALL', 'OPERA', 'EXPOSITION');
+
+CREATE TABLE Event (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description VARCHAR(1573) NOT NULL,
+    eventType EventType
+);
+
+CREATE TYPE TicketType AS ENUM ('VIP', 'USUAL', 'BUDGETARY', 'CHEAP');
+
+CREATE TABLE Ticket (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    x FLOAT NOT NULL,
+    y FLOAT NOT NULL CHECK ( y <= 654.00 ),
+    creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    price FLOAT NOT NULL CHECK ( price > 0.00 ),
+    ticketType TicketType NOT NULL,
+    event_id INT REFERENCES Event(id)
+);
+
